4525fdf6e393f595e77f01333086090d440978b0
Javier Telio Zapot <jtelio118@gmail.com>
2021-06-03 20:13:10 -0500

    add personal repositorie

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:javiertelioz/profile.git master:gh-pages
 
 cd -
